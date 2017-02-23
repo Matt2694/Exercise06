6486cd15850417b1f437687e137717a1b4cb07a6
Matthew Anthony Peterson <matt2694@edu.eal.dk>
2017-02-23 11:21:25 +0100

    Completed exercise for Thursday

--- LINQ_Examples/06_01_Lambda_Syntax_start.cs
+++ LINQ_Examples/06_01_Lambda_Syntax_start.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+//using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,6 +16,15 @@ namespace LINQ_Examples
             public string State { get; set; }
             public double Price { get; set; }
             public string[] Purchases { get; set; }
+            //public int CompareTo(Customer compareCustomer)
+            //{
+            //    if (x.ToString().Length == y.ToString().Length)
+            //        return string.Compare(x.ToString(), y.ToString());
+            //    else if (x.ToString().Length > y.ToString().Length)
+            //        return 1;
+            //    else
+            //        return -1;
+            //}
         }
 
         public class Distributor
@@ -101,7 +110,8 @@ namespace LINQ_Examples
 
         static void Main(string[] args)
         {
-            //exercise 1
+            //exercise 1-tue
+
             //IEnumerable<double> exchangeQuery =
             //    from e in ExchangedPrices
             //    where e < 1000
@@ -112,7 +122,8 @@ namespace LINQ_Examples
             //    where c.State == "GA"
             //    select c;
 
-            //exercise 2
+            //exercise 2-tue
+
             //IEnumerable<Customer> firstNameQuery =
             //    from c in customers
             //    select c;
@@ -127,14 +138,16 @@ namespace LINQ_Examples
             //    where d.State.Equals(c.State)
             //    select c.State;
 
-            //exercise 3
+            //exercise 3-tue
+
             //var takeQuery = customers
             //    .Take(3);
 
             //var takeORQuery = customers
             //    .Where (c => c.State.Equals("OR")).Take(3);
 
-            //exercise 4
+            //exercise 4-tue
+
             //var orderByFNameQuery = customers
             //    .OrderBy(c => c.First);
 
@@ -147,20 +160,23 @@ namespace LINQ_Examples
             //var orderByLOfFirstLastQuery = customers
             //    .OrderBy(c => c.First.Length).ThenBy(c => c.Last);
 
-            //exercise 5
+            //exercise 5-tue
+
             //IEnumerable<IGrouping<char, Customer>> groupByFirstLetterQuery = customers
             //    .GroupBy(c => c.First.First());
 
             //IEnumerable<IGrouping<string, Customer>> groupByFirstLetterQuery = customers
             //      .GroupBy(c => c.State);
 
-            //exercise 6
+            //exercise 6-tue
+
             //var exceptFindQuery = customers
             //    .Where(c => c.First.First() == c.Last.First());
 
             //var exceptQuery = customers.Except(exceptFindQuery);
 
-            //exercise 7
+            //exercise 7-tue
+
             //bool anyQuery = customers.Any(c => c.First.Contains("ed"));
 
             //foreach (var c in exceptQuery)
@@ -180,7 +196,266 @@ namespace LINQ_Examples
             //    }
             //}
 
+            //exercise 1-thur
+
+            //List<double> custLT100 = new List<double>();
+            //foreach(double exPrice in ExchangedPrices)
+            //{
+            //    if(exPrice < 1000)
+            //    {
+            //        custLT100.Add(exPrice);
+            //    }
+            //}
+
+            //List<Customer> custGA = new List<Customer>();
+            //foreach(Customer cus in customers)
+            //{
+            //    if(cus.State == "GA")
+            //    {
+            //        custGA.Add(cus);
+            //    }
+            //}
+
+            //exercise 2-thur
+
+            //List<Customer> custFN = new List<Customer>();
+            //foreach(Customer cus in customers)
+            //{
+            //    custFN.Add(cus);
+            //}
+
+            //List<string> FullName = new List<string>();
+            //foreach(Customer cus in customers)
+            //{
+            //    FullName.Add(cus.First + " " + cus.Last);
+            //}
+
+            //List<string> cusDisState = new List<string>();
+            //foreach(Customer cus in customers)
+            //{
+            //    foreach(Distributor dis in distributors)
+            //    {
+            //        if (cus.State.Equals(dis.State))
+            //        {
+            //            cusDisState.Add(cus.State);
+            //        }
+            //    }
+            //}
+
+            //exercise 3-thur
+
+            //List<Customer> first3Cust = new List<Customer>();
+            //for(int i = 0; i < 3; i++)
+            //{
+            //    first3Cust.Add(customers[i]);
+            //}
+
+            //List<Customer> first3Cust = new List<Customer>();
+            //foreach (Customer cus in customers)
+            //{
+            //    if (first3Cust.Count < 3)
+            //    {
+            //        if (cus.State.Equals("OR"))
+            //        {
+            //            first3Cust.Add(cus);
+            //        }
+            //    }
+            //}
+
+            //exercise 4-thur
+
+            //List<string> fNAlpha = new List<string>();
+            //foreach(Customer cus in customers)
+            //{
+            //    fNAlpha.Add(cus.First);
+            //}
+            //fNAlpha.Sort();
+
+            //List<string> lNLength = new List<string>();
+            //foreach(Customer cus in customers)
+            //{
+            //    lNLength.Add(cus.Last);
+            //}
+            //lNLength.Sort(CompareFirstNamesByLengthEx4);
+
+            //List<Customer> sortedByPrice = new List<Customer>();
+            //foreach(Customer cus in customers)
+            //{
+            //    sortedByPrice.Add(cus);
+            //}
+            //sortedByPrice.Sort(CompareCustomersByPriceEx4);
+
+            //List<Customer> sortedByFirstLengthLastAlpha = new List<Customer>();
+            //foreach (Customer cus in customers)
+            //{
+            //    sortedByFirstLengthLastAlpha.Add(cus);
+            //}
+            //sortedByFirstLengthLastAlpha.Sort(CompareCustomersByFirstAndLastEx4);
+
+            //exercise 5-thur
+
+            //List<Customer> cusGroupByFirstLetter = new List<Customer>();
+            //List<char> firstLetter = new List<char>();
+            //foreach(Customer cus in customers)
+            //{
+            //    cusGroupByFirstLetter.Add(cus);
+            //}
+            //cusGroupByFirstLetter.Sort(CompareCustomersByFirstAlphaEx5);
+            //foreach(Customer cus in cusGroupByFirstLetter)
+            //{
+            //    if (!firstLetter.Contains(cus.First[0]))
+            //    {
+            //        firstLetter.Add(cus.First[0]);
+            //    }
+            //}
+            //foreach(char n in firstLetter)
+            //{
+            //    Console.WriteLine(n);
+            //    foreach(Customer cus in cusGroupByFirstLetter)
+            //    {
+            //        if (n.Equals(cus.First[0]))
+            //        {
+            //            Console.WriteLine(cus.First + " " + cus.Last);
+            //        }
+            //    }
+            //}
+
+            //List<Customer> cusGroupByState = new List<Customer>();
+            //List<string> states = new List<string>();
+            //foreach(Customer cus in customers)
+            //{
+            //    cusGroupByState.Add(cus);
+            //}
+            //cusGroupByState.Sort(CompareCustomersByState);
+            //foreach(Customer cus in cusGroupByState)
+            //{
+            //    if (!states.Contains(cus.State))
+            //    {
+            //        states.Add(cus.State);
+            //    }
+            //}
+            //foreach (string n in states)
+            //{
+            //    Console.WriteLine(n);
+            //    foreach (Customer cus in cusGroupByState)
+            //    {
+            //        if (n.Equals(cus.State))
+            //        {
+            //            Console.WriteLine(cus.First + " " + cus.Last);
+            //        }
+            //    }
+            //}
+
+            //exercise 6-thur
+
+            //List<Customer> cusUsingExcept = new List<Customer>();
+            //List<Customer> passingCustomers = new List<Customer>();
+            //foreach(Customer cus in customers)
+            //{
+            //    cusUsingExcept.Add(cus);
+            //}
+            //foreach(Customer cus1 in cusUsingExcept)
+            //{
+            //    int i = 0;
+            //    foreach (Customer cus2 in cusUsingExcept)
+            //    {
+            //        if (cus1.First[0].Equals(cus2.Last[0]))
+            //        {
+            //            i++;
+            //        }
+            //    }
+            //    if (i == 0)
+            //    {
+            //        passingCustomers.Add(cus1);
+            //    }
+            //}
+
+            //exercise 7-thur
+
+            //List<Customer> cusUsingAny = new List<Customer>();
+            //bool anyQuery = false;
+            //foreach(Customer cus in customers)
+            //{
+            //    cusUsingAny.Add(cus);
+            //}
+            //foreach(Customer cus in cusUsingAny)
+            //{
+            //    if (cus.First.Contains("ed"))
+            //    {
+            //        anyQuery = true;
+            //    }
+            //}
+            //Console.Write("There {0} customers with \"ed\" in their first name.",
+            //              anyQuery ? "are" : "are not any");
+
+
+            //foreach (Customer n in passingCustomers)
+            //{
+            //    Console.WriteLine(n.First + " " + n.Last);
+            //}
+
             Console.ReadKey();
         }
+
+        //private static int CompareFirstNamesByLengthEx4(string x, string y)
+        //{
+        //    if (x == null)
+        //    {
+        //        if (y == null)
+        //        {
+        //            return 0;
+        //        }
+        //        else
+        //        {
+        //            return -1;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        if (y == null)
+        //        {
+        //            return 1;
+        //        }
+        //        else
+        //        {
+        //            int retval = x.Length.CompareTo(y.Length);
+
+        //            if (retval != 0)
+        //            {
+        //                return retval;
+        //            }
+        //            else
+        //            {
+        //                return x.CompareTo(y);
+        //            }
+        //        }
+        //    }
+        //}
+
+        //private static int CompareCustomersByPriceEx4(Customer x, Customer y)
+        //{
+        //    return y.Price.CompareTo(x.Price);
+        //}
+
+        //private static int CompareCustomersByFirstAndLastEx4(Customer x, Customer y)
+        //{
+        //    int returnValue;
+        //    returnValue = x.First.Length.CompareTo(y.First.Length);
+        //    if (returnValue == 0)
+        //    {
+        //        returnValue = x.Last.CompareTo(y.Last);
+        //    }
+        //    return returnValue;
+        //}
+
+        //private static int CompareCustomersByFirstAlphaEx5(Customer x, Customer y)
+        //{
+        //    return x.First.CompareTo(y.First);
+        //}
+
+        //private static int CompareCustomersByState(Customer x, Customer y)
+        //{
+        //    return x.State.CompareTo(y.State);
+        //}
     }
 }
